708dac3be3b741d6d6a292724fa9f13520015931
Bardell <Bardellwilson7@gmail.com>
2022-06-11 12:26:07 -0500

    day 3 finished

---
+++ api/tags.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const tagsRouter = express.Router();
+const { getAllTags, getPostsByTagName } = require("../db");
+
+tagsRouter.use((req, res, next) => {
+  console.log("A request is being made to /tags");
+
+  next();
+});
+
+tagsRouter.get("/", async (req, res) => {
+  try {
+    const tags = await getAllTags();
+
+    res.send({
+      tags,
+    });
+  } catch (error) {
+    throw error;
+  }
+});
+
+tagsRouter.get("/:tagName/posts", async (req, res, next) => {
+  // read the tagname from the params
+  const { tagName } = req.params;
+
+  try {
+    // use our method to get posts by tag name from the db
+    const posts = await getPostsByTagName(tagName);
+    // send out an object to the client { posts: // the posts }
+    if (posts) {
+      res.send(posts);
+    } else {
+      next({
+        name: "getting an error",
+        message: "fix this error",
+      });
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+module.exports = tagsRouter;
---
+++ index.js
@@ -0,0 +1,33 @@
+const { PORT = 3000 } = process.env;
+const express = require('express');
+const server = express();
+require('dotenv').config();
+
+// remove this once you confirm it works
+// console.log(process.env.JWT_SECRET);
+// like, seriously. go delete that!
+
+// EVERYTHING ELSE
+
+const { apiRouter } = require('./api');
+server.use('/api', apiRouter);
+
+const morgan = require('morgan');
+server.use(morgan('dev'));
+
+server.use(express.json())
+
+server.use((req, res, next) => {
+    console.log("<____Body Logger START____>");
+    console.log(req.body);
+    console.log("<_____Body Logger END_____>");
+  
+    next();
+  });
+
+const { client } = require("./db");
+client.connect();
+
+server.listen(PORT, () => {
+  console.log("The server is up on port", PORT);
+});
